dbd58a0aa4de5e9841fc11063ff419cbd6453fb9
SuneethaBagya <ksunitha3471@gmail.com>
2025-12-18 11:11:49 +0530

    Add files via upload
    Day 1 Assignment

---
+++ Day1/DataType.js
@@ -0,0 +1,10 @@
+var testerName = "Suneetha"
+var organisationName = "TestLeaf"
+var contactNumber = 12345
+var isAutomationTester = true
+var hasPlaywrightExperience
+console.log("testerName" + " " + typeof(testerName))
+console.log("organisationName" + " " + typeof(organisationName))
+console.log("contactNumber" + " " + typeof(contactNumber))
+console.log("isAutomationTester" + " " + typeof isAutomationTester)
+console.log("hasPlaywrightExperience" + " " + typeof(hasPlaywrightExperience))
