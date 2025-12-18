e60abab6a5c622edded14042487cff5a971d8422
SuneethaBagya <ksunitha3471@gmail.com>
2025-12-18 11:15:34 +0530

    Add files via upload
    Day3 Assignment

---
+++ Day3/PrimeOddNumbers.js
@@ -0,0 +1,6 @@
+let value = 20
+for(let i=0;i<=value;i++){
+    if(i%3==0){
+        console.log("It is odd number : ",i)
+    }
+}
---
+++ Day3/SwitchCase.js
@@ -0,0 +1,32 @@
+let day=4;
+switch (day) {
+    case 1:
+        console.log("Day is Sunday");
+        
+        break;
+    case 2:
+        console.log("Day is Monday");
+        
+        break;
+
+        case 3:
+        console.log("Day is Tuesday");
+        
+        break;
+
+        case 4:
+        console.log("Day is Wednesday");
+        
+        break;
+
+        case 5:
+        console.log("Day is Thursday");
+        
+        break;
+
+      
+
+    default:
+        console.log("wrong value")
+        break;
+}
